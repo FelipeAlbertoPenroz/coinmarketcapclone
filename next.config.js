2e233bed7c979b38243f34a8f62bb5d50b09f20e
Penroz <axpenrozfleipe@gmail.com>
2022-05-26 02:43:54 -0400

    ADD: Header frontend

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-<<<<<<< HEAD
   images: {
     domains: ['s2.coinmarketcap.com', 's3.coinmarketcap.com', 'encrypted-tbn0.gstatic.com']
-  },
-=======
-  reactStrictMode: true,
->>>>>>> 59f264ed8fc767da8359190209407a6cd9d52635
+  }
 }
